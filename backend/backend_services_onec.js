a77966a57d925e33fa0914ae6424657f4d3f569c
mihaNKR <admin@krasokno.com>
2025-07-09 10:35:22 +0700

    Update backend_services_onec.js

--- backend/backend_services_onec.js
+++ backend/backend_services_onec.js
@@ -1,1 +1,31 @@
 
+const axios = require('axios');
+const { odata_url, login, password } = require('./config/onec.json');
+
+/**
+ * Получить показания счётчиков пользователя
+ */
+async function getMeterReadings(userId) {
+  const res = await axios.get(
+    `${odata_url}ПоказанияСчетчиков?$filter=Владелец_Key eq guid'${userId}'`,
+    { auth: { username: login, password } }
+  );
+  return res.data.value;
+}
+
+/**
+ * Добавить новое показание
+ */
+async function addMeterReading({ userId, meterValue, date = new Date() }) {
+  await axios.post(
+    `${odata_url}ПоказанияСчетчиков`,
+    {
+      Владелец_Key: userId,
+      Дата: date.toISOString(),
+      Значение: meterValue
+    },
+    { auth: { username: login, password } }
+  );
+}
+
+module.exports = { getMeterReadings, addMeterReading };
